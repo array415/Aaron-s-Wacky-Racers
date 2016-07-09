d53f6fd759d510dbc44e894a9ccafb00a8a147e0
YOLANDA RODRIGUEZ <Array415@gmail.com>
2016-07-09 14:27:49 -0700

    adding html and css styling

--- script.js
+++ script.js
@@ -6,43 +6,51 @@ $(document).on('ready', function(){
   var playerTwo = new Player();
 
 
+  $(".wow").on('click', function(){
+    $('.playerOneName').hide();
+    $('.wow').hide();
+  });
 
-  $( 'body' ).on('keypress',function(event) {
-    if(event.which == 112){
-      playerOne.name = $('.frank').val();
-      console.log(playerOne);
-      $( ".trackOne" ).animate(
-        {'margin-left': '+=3%'},
-        150,
-        function complete() {
 
-        });
 
-        playerOne.increase();
-        if(playerOne.moves === 25){
-          console.log(playerOne.playerName + " " + "Wins!");
-        }
 
-      }
 
-      $(".trackOne").clearQueue();
-
-    });
-    //
-    // $(window).keypress(function(event){
-    //   if(event.which == 113){
-    //
-    //     $( ".trackTwo" ).animate(
-    //       {'margin-left': '+=3%'},
-    //       300 );
-    //     }
-    //     $(".trackTwo").clearQueue();
-    //   });
-    //
-    //
-    //
-    // });
-    //
+  // $( 'body' ).on('keypress',function(event) {
+  //   if(event.which == 112){
+  //     playerOne.name = $('.frank').val();
+  //     console.log(playerOne);
+  //     $( ".trackOne" ).animate(
+  //       {'margin-left': '+=3%'},
+  //       150,
+  //       function complete() {
+  //
+  //       });
+  //
+  //       playerOne.increase();
+  //       if(playerOne.moves === 25){
+  //         console.log(playerOne.playerName + " " + "Wins!");
+  //       }
+  //
+  //     }
+  //
+  //     $(".trackOne").clearQueue();
+  //
+  //   });
+  //   //
+  //   // $(window).keypress(function(event){
+  //   //   if(event.which == 113){
+  //   //
+  //   //     $( ".trackTwo" ).animate(
+  //   //       {'margin-left': '+=3%'},
+  //   //       300 );
+  //   //     }
+  //   //     $(".trackOne").clearQueue();
+  //   //   });
+  //   //
+  //   //
+  //   //
+  //   // });
+  //   //
 
     function Player(){
       this.moves = 0;
