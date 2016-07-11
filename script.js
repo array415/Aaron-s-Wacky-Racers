2cacc9291819364849ef742ec06ec4ff2a327e11
YOLANDA RODRIGUEZ <Array415@gmail.com>
2016-07-10 21:29:40 -0700

    adding final styling

--- script.js
+++ script.js
@@ -9,36 +9,44 @@ $(document).on('ready', function(){
   $('body').on('keypress',function(event) {
     if(event.which == 112){
       playerOne.condensedWinCheck();
-
+      playerOne.name = $('.playerOne').val();
       $( ".trackOne" ).animate(
         {'margin-left': '+='+playerOne.move+'%'},
         600);
 
-        if(playerOne.winner >= 100){
+        if(playerOne.winner >= 90){
           $('.announce').html("<h1>"+ playerOne.name + " " + "Wins!" + "</h1>");
           $('.announce').animate({'margin-left' : '70%'},
           9000);
+          event.preventDefault();
           playerOne.reset();
           $('.announceWinner').show();
         }
       }
-
-      $(".trackOne").clearQueue();
 
     });
 
     $('body').keypress(function(event){
       if(event.which == 113){
         playerTwo.condensedWinCheck();
+        playerTwo.name = $('.playerTwo').val();
         $( ".trackTwo" ).animate(
           {'margin-left': '+='+playerTwo.move+'%'},
           600 );
         }
-        $(".trackTwo").clearQueue();
       });
 
-    $('.avatar').on('click', function(){
+    $('.psuedoSubmit').on('click', function(){
+      $('.playerName').hide();
+    });
 
+    $('.avatar').on('click', function(){
+      $('.trackOne').css('margin-left', '0');
+      $('.trackTwo').css('margin-left', '0');
+      playerOne.reset();
+      playerTwo.reset();
+      $('.announceWinner').hide();
+      $('.playerName').show();
     });
 
 });
@@ -48,6 +56,7 @@ $(document).on('ready', function(){
       this.move = 0;
       this.name = null;
       this.winner = 0;
+      this.money = 1000;
 
       this.whoWon = function(){
         this.winner += this.move;
@@ -65,7 +74,6 @@ $(document).on('ready', function(){
       this.condensedWinCheck = function(){
         this.randomMove();
         this.whoWon();
-        this.name = $('.firstName').val();
       };
 
     }
